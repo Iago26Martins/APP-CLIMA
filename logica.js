45ef6021dad5d7d631e1a4dddd99205bad937d97
iago martins <iago.timbrasil@gmail.com>
2026-04-10 21:17:20 -0300

    clima2

--- logica.js
+++ logica.js
@@ -1,15 +1,28 @@
-async function buscarClimaname() {
+async function buscarClima() {
     const cidade = document.getElementById('cidade').value;
     const url = `https://goweather.xyz/v2/weather/${cidade}`;
 
     const resposta = await fetch(url);
     const dados = await resposta.json();
 
+    if(dados.message === "NOT_FOUND"){
+        document.getElementById("resultado").innerHTML = `
+        <p>Essa Cidade Não Existe<p/>
+   `;
+   return;
+}
+
+    // Caso de SUCESSO
     document.getElementById("resultado").innerHTML = `
     <h2>Clima em ${cidade}</h2>
     <p>Temperatua: ${dados.temperature}</p>
     <p>Vento: ${dados.wind}</p>
-    <p>Descrição: ${dados.descripition}</p>
+    <p>Descrição: ${dados.description}</p>
     `;
-
 }
+
+document.getElementById("cidade").addEventListener("keypress", function (event){
+    if (event.key === "Enter") {
+        buscarClima();
+    }
+});
